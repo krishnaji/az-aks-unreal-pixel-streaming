359273d9b12730f479f08253a94a7e32d9c390ec
Shrikrishna Khose <shrikrishnakhose@Shrikrishnas-MacBook-Pro.local>
2022-11-04 22:56:26 -0400

    Update Docker context paths

--- docker-build.sh
+++ docker-build.sh
@@ -5,7 +5,7 @@ cd Matchmaker/platform_scripts/bash
 docker build -t matchmaker:4.27 -f Dockerfile .
 # Build Signalling image
 cd SignallingWebServer/platform_scripts/bash/
-docker build -t signallingwebserver:4.27 -f Dockerfile .
+docker build -t signallingwebserver:4.27 -f ./Dockerfile ../..
 # Build Unreal Engine App image . Copy the Dockerfile to the root of the Unreal Engine App
 cd Game/Dockerfile
 docker build -t game:4.27 -f Dockerfile .
